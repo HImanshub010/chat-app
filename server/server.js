b0d491f2ff4110460dd2c4d2cbdf00e0d2869e48
himanshub010 <himanshub010.com>
2019-03-13 16:04:56 +0530

    greet new user and notify others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,28 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('user connected');
-
+  socket.emit('newMessage',{
+    from : 'Admin',
+    text : 'Welcome to chat aPp',
+    createdAt : new Date().getTime()
+  }) 
+  socket.broadcast.emit('newMessage',{
+    from : 'Admin',
+    text : 'New user joined ',
+    createdAt : new Date().getTime()
+  })
   socket.on('createMessage',(message)=>{
     console.log('messageCreated '+ message.from);
     io.emit('newMessage',{
       from : message.from,
       text : message.text,
       createdAt : new Date().getTime()
-    })
+    }) 
+    // socket.broadcast.emit('newMessage',{
+    //   from : message.from,
+    //   text : message.text,
+    //   createdAt : new Date().getTime()
+    // })
   });
 
   socket.on('disconnect',()=>{
