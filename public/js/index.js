30bd2f81db8ff621e6bc62bf6fbb19e8131c56e6
himanshub010 <himanshub010.com>
2019-03-14 11:44:56 +0530

    set geolaction,styles

--- public/js/index.js
+++ public/js/index.js
@@ -14,17 +14,47 @@ socket.on('newMessage',(message)=>{
   jQuery('#messages').append(li);
 })
 
+socket.on('newLocationMessage',(message)=>{
+  let li = jQuery('<li></li>')
+  let a = jQuery('<a target ="_blank"> My current location</a>')
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+
+})
+
 $(document).ready(function(){
   jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
     console.log('working');
+    let messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage',{
       from : 'User',
-      text : jQuery('[name=message]').val()
+      text : messageTextBox.val()
     },()=>{
-  
+      messageTextBox.val('')
     }) 
   });
+
+  let locationButton = jQuery('#send-location');
+  locationButton.on('click',()=>{
+    if(!navigator.geolocation){
+      return alert('Geoloation not supported by your browser!!');
+    }
+    locationButton.attr('disabled','disabled').text('Sending location ...');
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position); 
+      locationButton.removeAttr('disabled').text('Send location');
+      socket.emit('createLocationMessage',{
+        latitude: position.coords.latitude,
+        longitude:position.coords.longitude
+      })
+    },()=>{
+      locationButton.removeAttr('disabled').text('Send location');
+      alert('Unable to fetch Location');
+    })
+  })
 });
 
  
